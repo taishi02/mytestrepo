4eac0a4d9def4ae1920a18ca14c9d8be051628e1
Taishi Iwasaki <taishi@google.com>
2014-04-16 16:29:16 +0900

    test deployment

--- helloworld.py
+++ helloworld.py
@@ -2,6 +2,6 @@ import webapp2
 
 class HelloWorldHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write('Hello, World!\n')
+        self.response.out.write('Hello, World!\n\n')
 
 app = webapp2.WSGIApplication([('/', HelloWorldHandler)])
